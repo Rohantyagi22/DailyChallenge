843c16fb1e265bd4ae22f2c67d12a4768c45436d
Rohantyagi22 <64071286+Rohantyagi22@users.noreply.github.com>
2026-04-24 23:48:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,31 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while(i>=0 &&nums[i]>=nums[i+1]) //just finding one greater 
+        {
+            i--;
+        }
+        int j = nums.length - 1;
+        if(i>=0){
+            while(j>i && nums[j]<nums[i]) //just greater
+            {
+                j--;
+            }
+            swap(nums,i,j);
+        }
+        reverse(nums,i+1);
+    }
+    public void swap(int[] nums,int i,int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    public void reverse(int[] nums,int i){
+        int j = nums.length - 1;
+        while(i<=j){
+            swap(nums,i,j);
+            i++;
+            j--;
+        }
+    }
+}
