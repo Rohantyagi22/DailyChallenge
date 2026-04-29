48524b90775503e4af971e2ff847039e81c24767
Rohantyagi22 <64071286+Rohantyagi22@users.noreply.github.com>
2026-04-29 06:41:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,24 +1,29 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-       Set<Integer> set = new HashSet<>();
-       int left = 0;
-       long sum = 0, maxSum = 0;
-       for(int right = 0 ; right<nums.length;right++){
-        while(set.contains(nums[right])){
-            set.remove(nums[left]);
-            sum=sum-nums[left];
-            left++;
-        }
-        set.add(nums[right]);
-        sum+=nums[right];
-        
-        if(right-left+1 == k){
-                maxSum = Math.max(sum,maxSum);
+        Set<Integer> set = new HashSet<>();
+        int left = 0;
+        long sum=0, maxSum = 0;
+        for(int right = 0;right<nums.length;right++){
+            //remove duplicates
+            while(set.contains(nums[right])){
                 set.remove(nums[left]);
-                sum=sum-nums[left];
+                sum-=nums[left];
                 left++;
             }
-       }
-       return maxSum; 
+            //Add numbers from right
+            set.add(nums[right]);
+            sum+=nums[right];
+          
+
+            //check for sliding window
+            if(right-left+1 == k){
+                maxSum = Math.max(sum,maxSum);
+                set.remove(nums[left]);
+                sum-=nums[left];
+                left++;
+            }
+
+        }
+        return maxSum;
     }
 }
