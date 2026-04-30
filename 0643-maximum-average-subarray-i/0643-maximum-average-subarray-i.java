efb16a337517fde6a2e32412bb731d6870ed45e9
Rohantyagi22 <64071286+Rohantyagi22@users.noreply.github.com>
2026-04-30 21:11:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -3,17 +3,14 @@ class Solution {
         double sum = 0;
         for(int i=0;i<k;i++){
             sum += nums[i];
-            System.out.println("sum:"+sum);
         }
         double maxSum = sum;
         for(int i=k;i<nums.length;i++){
             sum = sum + nums[i] - nums[i-k];
             if(sum>maxSum){
                 maxSum = sum;
-                System.out.println("maxsum: "+maxSum);
             }
         }
-        System.out.println("maxSum/k: "+maxSum/k);
         return maxSum/k;
     }
 }
