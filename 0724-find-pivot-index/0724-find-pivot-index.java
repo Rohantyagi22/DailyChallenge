0f665320a57bc64b5363a28e6b160f4bbcc8b1ea
Rohantyagi22 <64071286+Rohantyagi22@users.noreply.github.com>
2026-05-10 12:00:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -5,10 +5,7 @@ class Solution {
             sr+=nums[i];
         }
         for(int i=0;i<nums.length;i++){
-            System.out.print("i "+i);
             sr = sr  - nums[i];
-            System.out.print(" sr "+sr);
-            System.out.println(" sl "+sl);
             if(sl == sr) return i;
             sl +=nums[i];
         }
