08061bc583ad5df570a3210b725ce92f271212b6
Rohantyagi22 <64071286+Rohantyagi22@users.noreply.github.com>
2026-05-13 21:28:01 +0530

    Time: 2 ms (95.44%), Space: 43.4 MB (80.9%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -2,19 +2,24 @@ class Solution {
     public int calPoints(String[] operations) {
         int sum = 0;
         Stack<Integer> st = new Stack<>();
-        for(int i=0;i<operations.length;i++){
-            if(operations[i].equals("+")){
-                st.push(st.get(st.size()-1)+st.get(st.size()-2));
-            }else if(operations[i].equals("D")){
-                st.push(st.get(st.size()-1)*2);
-            }else if(operations[i].equals("C")){
-                st.pop();
+        for(String op: operations){
+            if(op.equals("+")){
+                int top = st.pop();
+                int newScore = top + st.peek();
+                st.push(top);
+                st.push(newScore);
+                sum += newScore;
+            }else if(op.equals("D")){
+                int newScore =(st.peek()*2);
+                st.push(newScore);
+                sum += newScore;
+            }else if(op.equals("C")){
+                sum -= st.pop();;
             }else{
-                st.push(Integer.parseInt(operations[i]));
+                int score = Integer.parseInt(op);
+                st.push(score);
+                sum += score;
             }
-        }
-        while(!st.isEmpty()){
-            sum += st.pop();
         }
         return sum;
     }
