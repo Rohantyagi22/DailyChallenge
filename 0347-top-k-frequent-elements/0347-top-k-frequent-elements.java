ec7aeb4c4bc279b500e583ddd7fe29fe45a42cf5
Rohantyagi22 <64071286+Rohantyagi22@users.noreply.github.com>
2026-06-25 16:19:42 +0530

    Time: 16 ms (40.17%), Space: 47.4 MB (90.64%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -4,9 +4,9 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)-> map.get(b)-map.get(a));
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->map.get(b)-map.get(a));
         pq.addAll(map.keySet());
-        int result[] = new int[k];
+        int[] result = new int[k];
         for(int i=0;i<k;i++){
             result[i] = pq.poll();
         }
