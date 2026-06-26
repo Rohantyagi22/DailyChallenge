f15d5dfadf7600e738e6832d852129f4ae64a87a
Rohantyagi22 <64071286+Rohantyagi22@users.noreply.github.com>
2026-06-26 13:35:15 +0530

    Time: 9 ms (92.22%), Space: 46.7 MB (71.07%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,27 +1,28 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> result = new ArrayList<>();
-        int[] sArr = new int[26];
-        int[] pArr = new int[26];
+        int[] s1 = new int[26];
+        int[] s2 = new int[26];
+        int k = p.length();
         for(int i=0;i<p.length();i++){
-            pArr[p.charAt(i)-'a']++;
+            s1[p.charAt(i)-'a']++;
         }
+        List<Integer> result = new ArrayList<>();
         int left = 0;
-        for(int right = 0; right<s.length();right++){
-            sArr[s.charAt(right)-'a']++;
-            if(right - left + 1 > p.length()){
-                sArr[s.charAt(left)-'a']--;
-                left++;
+        for(int right = 0; right< s.length(); right++){
+            s2[s.charAt(right)-'a']++;
+            if(right-left+1>k){
+                s2[s.charAt(left)-'a']--;
+                left++;    
             }
-            if(matched(sArr,pArr)){
+            if(isMatched(s1,s2)){
                 result.add(left);
             }
         }
         return result;
     }
-    public boolean matched(int[] sArr,int[] pArr){
-        for(int i=0;i<sArr.length;i++){
-            if(sArr[i]!=pArr[i]){
+    public boolean isMatched(int[] s1,int[] s2){
+        for(int i=0;i<s1.length;i++){
+            if(s1[i]!=s2[i]){
                 return false;
             }
         }
