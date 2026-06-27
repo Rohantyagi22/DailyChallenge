bce001dcd40a5eaef81388b08357ea9a302441d7
Rohantyagi22 <64071286+Rohantyagi22@users.noreply.github.com>
2026-06-27 14:15:51 +0530

    Time: 7 ms (68.04%), Space: 44 MB (61.91%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,30 +1,26 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if(s2.length()<s1.length()){
-            return false;
-        }
         int[] s1Arr = new int[26];
         int[] s2Arr = new int[26];
         for(int i=0;i<s1.length();i++){
             s1Arr[s1.charAt(i)-'a']++;
         }
         int left = 0;
-        int k=s1.length();
-        for(int i=0;i<s2.length();i++){
-            s2Arr[s2.charAt(i)-'a']++;
-            if(i-left+1>k){
+        for(int right=0;right<s2.length();right++){
+            s2Arr[s2.charAt(right)-'a']++;
+            if(right>=s1.length()){
                 s2Arr[s2.charAt(left)-'a']--;
                 left++;
             }
-            if(isMatched(s1Arr,s2Arr)){
+            if(isMatch(s1Arr,s2Arr)){
                 return true;
             }
         }
         return false;
     }
-    public boolean isMatched(int[] s1, int[] s2){
-        for(int i=0;i<s1.length;i++){
-            if(s1[i]!=s2[i]){
+    public boolean isMatch(int[] s1Arr,int[] s2Arr){
+        for(int i=0;i<s1Arr.length;i++){
+            if(s1Arr[i]!=s2Arr[i]){
                 return false;
             }
         }
