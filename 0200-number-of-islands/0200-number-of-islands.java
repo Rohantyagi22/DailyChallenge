5fcbef181134ef02cc2e7aca7db904375059dbb4
Rohantyagi22 <64071286+Rohantyagi22@users.noreply.github.com>
2026-07-01 19:48:39 +0530

    Time: 8 ms (10.72%), Space: 51.3 MB (92.3%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,27 +1,32 @@
 class Solution {
     public int numIslands(char[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int[][] directions = {{-1,0},{1,0},{0,1},{0,-1}};
+        Queue<int[]> queue = new LinkedList<>();
         int count = 0;
-        int row = grid.length;
-        int col = grid[0].length;
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
                 if(grid[i][j]=='1'){
                     count++;
-                    dfs(grid,i,j);
+                    queue.offer(new int[]{i,j});
+                    grid[i][j] = '0';
+                    while(!queue.isEmpty()){
+                        int[] cell = queue.poll();
+                        int row = cell[0];
+                        int col = cell[1];
+                        for(int[] dir: directions){
+                            int newRow = dir[0]+row;
+                            int newCol = dir[1]+col;
+                            if(newRow>=0 && newCol>=0 && newRow<rows && newCol<cols && grid[newRow][newCol]=='1'){
+                                queue.offer(new int[]{newRow,newCol});
+                                grid[newRow][newCol]='0';
+                            }
+                        }
+                    }
                 }
             }
         }
         return count;
     }
-    public void dfs(char[][] grid,int row,int col){
-            //Boundary check
-            if(row>=grid.length || col>=grid[0].length || row<0 || col<0 || grid[row][col]=='0'){
-                return;
-            }
-            grid[row][col] = '0';
-            dfs(grid,row-1,col); //up
-            dfs(grid,row+1,col); //down
-            dfs(grid,row,col-1); //left
-            dfs(grid,row,col+1); //right
-        }
 }
