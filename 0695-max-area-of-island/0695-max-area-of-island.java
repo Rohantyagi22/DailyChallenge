ba6316ceb8751ad597011a78f2c186be6f4f20bd
Rohantyagi22 <64071286+Rohantyagi22@users.noreply.github.com>
2026-07-04 13:19:54 +0530

    Time: 3 ms (25.4%), Space: 46.4 MB (83.21%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -3,25 +3,35 @@ class Solution {
         int rows = grid.length;
         int cols = grid[0].length;
         int maxArea = 0;
+        Queue<int[]> q = new LinkedList<>();
+        int[][] directions = {
+            {1,0},
+            {-1,0},
+            {0,1},
+            {0,-1}
+        };
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
                 if(grid[i][j]==1){
-                    maxArea = Math.max(maxArea,dfs(grid,i,j));
+                    int area = 0;
+                    q.offer(new int[]{i,j});
+                    grid[i][j]=0;
+                    while(!q.isEmpty()){
+                        int[] cell = q.poll();
+                        area++;
+                        for(int[] dir:directions){
+                            int nr = cell[0]+dir[0];
+                            int nc = cell[1]+dir[1];
+                            if(nr>=0 && nc>=0 && nr<rows && nc<cols && grid[nr][nc]==1){
+                                grid[nr][nc] = 0;
+                                q.offer(new int[]{nr,nc});
+                            }
+                        }
+                    }
+                    maxArea = Math.max(maxArea,area);
                 }
             }
         }
         return maxArea;
-    }
-    public int dfs(int[][] grid,int row,int col){
-        if(row<0 || col<0 || row>grid.length-1 || col>grid[0].length-1 || grid[row][col]==0){
-            return 0;
-        }
-        grid[row][col] = 0;
-        int area = 1;
-        area+=dfs(grid,row-1,col);  //  left
-        area+=dfs(grid,row+1,col);  //  right
-        area+=dfs(grid,row,col-1);  //  up
-        area+=dfs(grid,row,col+1);  //  down
-        return area;
     }
 }
