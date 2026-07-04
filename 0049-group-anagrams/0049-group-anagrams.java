06e316fb9b52e5fdec79f8f2c202123652253923
Rohantyagi22 <64071286+Rohantyagi22@users.noreply.github.com>
2026-07-04 13:35:58 +0530

    Time: 8 ms (36.39%), Space: 49.9 MB (34.09%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String,List<String>> map = new HashMap<>();
-        for(String str:strs){
+        for(String str: strs){
             char[] ch = str.toCharArray();
             Arrays.sort(ch);
             String key = new String(ch);
