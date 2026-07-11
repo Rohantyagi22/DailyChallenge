49e97dc2873f8d4b4f5223788f1a53a4536f53be
Rohantyagi22 <64071286+Rohantyagi22@users.noreply.github.com>
2026-07-11 14:35:17 +0530

    Time: 139 ms (44.6%), Space: 279.6 MB (66.75%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,10 +1,11 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
+        //create adjancency list
         List<List<Integer>> graph = new ArrayList<>();
         for(int i=0;i<n;i++){
             graph.add(new ArrayList<>());
         }
-        for(int[] edge : edges){
+        for(int[] edge:edges){
             graph.get(edge[0]).add(edge[1]);
             graph.get(edge[1]).add(edge[0]);
         }
@@ -12,15 +13,15 @@ class Solution {
         return dfs(visited,graph,source,destination);
     }
     public boolean dfs(boolean[] visited,List<List<Integer>> graph,int source,int destination){
+        visited[source] = true;
         if(source == destination){
             return true;
         }
-        visited[source] = true;
         for(int neigh: graph.get(source)){
             if(!visited[neigh]){
-                if(dfs(visited,graph,neigh,destination)){
+                 if(dfs(visited,graph,neigh,destination)){
                     return true;
-                }
+                 }
             }
         }
         return false;
