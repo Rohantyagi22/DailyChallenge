0e902d40fb37b5ce7b1f5901c2c416e221e9c9a4
Rohantyagi22 <64071286+Rohantyagi22@users.noreply.github.com>
2026-07-11 18:31:33 +0530

    Time: 1 ms (100%), Space: 46.5 MB (68.56%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -2,36 +2,27 @@ class Solution {
     public int maxAreaOfIsland(int[][] grid) {
         int rows = grid.length;
         int cols = grid[0].length;
-        int maxArea = 0;
-        Queue<int[]> q = new LinkedList<>();
-        int[][] directions = {
-            {1,0},
-            {-1,0},
-            {0,1},
-            {0,-1}
-        };
+        int area = 0, maxArea = 0;
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
                 if(grid[i][j]==1){
-                    int area = 0;
-                    q.offer(new int[]{i,j});
-                    grid[i][j]=0;
-                    while(!q.isEmpty()){
-                        int[] cell = q.poll();
-                        area++;
-                        for(int[] dir:directions){
-                            int nr = cell[0]+dir[0];
-                            int nc = cell[1]+dir[1];
-                            if(nr>=0 && nc>=0 && nr<rows && nc<cols && grid[nr][nc]==1){
-                                grid[nr][nc] = 0;
-                                q.offer(new int[]{nr,nc});
-                            }
-                        }
-                    }
-                    maxArea = Math.max(maxArea,area);
+                    area = dfs(i,j,grid);
+                    maxArea = Math.max(area,maxArea);
                 }
             }
         }
         return maxArea;
+    }
+    public int dfs(int row,int col,int[][] grid){
+        if(row<0 || col<0 || row>=grid.length || col>=grid[0].length || grid[row][col] == 0){
+            return 0;
+        }
+        grid[row][col] = 0;
+        int area = 1;
+        area+=dfs(row+1,col,grid);
+        area+=dfs(row-1,col,grid);
+        area+=dfs(row,col+1,grid);
+        area+=dfs(row,col-1,grid);
+        return area;
     }
 }
