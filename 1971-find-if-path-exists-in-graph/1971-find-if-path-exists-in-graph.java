2e8c41b937d08a86d7f6749d067d8a05637f8404
Rohantyagi22 <64071286+Rohantyagi22@users.noreply.github.com>
2026-07-19 18:51:41 +0530

    Time: 152 ms (32.7%), Space: 279.3 MB (73.14%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,27 +1,25 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        //create adjancency list
-        List<List<Integer>> graph = new ArrayList<>();
+        List<List<Integer>> list = new ArrayList<>(); 
         for(int i=0;i<n;i++){
-            graph.add(new ArrayList<>());
+            list.add(new ArrayList<>());
         }
-        for(int[] edge:edges){
-            graph.get(edge[0]).add(edge[1]);
-            graph.get(edge[1]).add(edge[0]);
+        for(int[] edge: edges){
+            list.get(edge[0]).add(edge[1]);
+            list.get(edge[1]).add(edge[0]);
         }
         boolean[] visited = new boolean[n];
-        return dfs(visited,graph,source,destination);
+        return validPath(visited,list,source,destination);
     }
-    public boolean dfs(boolean[] visited,List<List<Integer>> graph,int source,int destination){
+    public boolean validPath(boolean[] visited,List<List<Integer>> list,int source,int destination){
         visited[source] = true;
-        if(source == destination){
-            return true;
-        }
-        for(int neigh: graph.get(source)){
-            if(!visited[neigh]){
-                 if(dfs(visited,graph,neigh,destination)){
+        if(source == destination) return true;
+        
+        for(int node: list.get(source)){
+            if(!visited[node]){
+                if(validPath(visited,list,node,destination)){
                     return true;
-                 }
+                }
             }
         }
         return false;
