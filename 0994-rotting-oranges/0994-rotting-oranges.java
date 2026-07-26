f4191068ccb2f92d6e52a3bbd59dfd15aa724a9d
Rohantyagi22 <64071286+Rohantyagi22@users.noreply.github.com>
2026-07-26 13:32:48 +0530

    Time: 4 ms (4.76%), Space: 43.9 MB (85.3%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,14 +1,12 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int row = grid.length;
-        int col = grid[0].length;
+        int count = 0;
         int fresh = 0;
-        int min = 0;
-        Queue<int[]> q = new LinkedList<>(); 
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
+        Queue<int[]> q= new LinkedList<>();
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j]==2){
-                    q.add(new int[]{i,j});
+                    q.offer(new int[]{i,j});
                 }else if(grid[i][j]==1){
                     fresh++;
                 }
@@ -17,27 +15,27 @@ class Solution {
         if(fresh == 0){
             return 0;
         }
-        int[][] dir ={
-            {1,0},{-1,0},{0,1},{0,-1}
-        };
+        int[][] dir = {{-1,0},{1,0},{0,1},{0,-1}};
         while(!q.isEmpty()){
-            int size = q.size();
+            int size=q.size();
             for(int i=0;i<size;i++){
                 int[] cell = q.poll();
+                int row = cell[0];
+                int col = cell[1];
                 for(int[] d:dir){
-                    int nrow = d[0]+cell[0];
-                    int ncol = d[1]+cell[1];
-                    if(nrow>=0 && ncol>=0 && nrow<grid.length && ncol<grid[0].length && grid[nrow][ncol]==1){
-                        grid[nrow][ncol] = 2;
+                    int nrow = row+d[0];
+                    int ncol = col+d[1];
+                    if(nrow>=0 && nrow<grid.length && ncol>=0 && ncol<grid[0].length && grid[nrow][ncol]==1){
                         fresh--;
+                        grid[nrow][ncol]=2;
                         q.offer(new int[]{nrow,ncol});
                     }
                 }
             }
             if(!q.isEmpty()){
-                min++;
+                count++;
             }
         }
-        return fresh == 0 ? min:-1;
+        return fresh == 0 ? count:-1;
     }
 }
