396a5acec6e626c2b8663864f41a652a6767dce4
Rohantyagi22 <64071286+Rohantyagi22@users.noreply.github.com>
2026-08-09 18:59:51 +0530

    Time: 170 ms (19.66%), Space: 279.4 MB (75.52%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,25 +1,24 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        List<List<Integer>> list = new ArrayList<>(); 
+        List<List<Integer>> li = new ArrayList<>();
         for(int i=0;i<n;i++){
-            list.add(new ArrayList<>());
+            li.add(new ArrayList<>());
         }
         for(int[] edge: edges){
-            list.get(edge[0]).add(edge[1]);
-            list.get(edge[1]).add(edge[0]);
+            li.get(edge[0]).add(edge[1]);
+            li.get(edge[1]).add(edge[0]);
         }
         boolean[] visited = new boolean[n];
-        return validPath(visited,list,source,destination);
+        return valid(visited,li,source,destination);
     }
-    public boolean validPath(boolean[] visited,List<List<Integer>> list,int source,int destination){
+    public boolean valid(boolean[] visited,List<List<Integer>> li,int source,int destination){
         visited[source] = true;
         if(source == destination) return true;
-        
-        for(int node: list.get(source)){
-            if(!visited[node]){
-                if(validPath(visited,list,node,destination)){
+        for(int i:li.get(source)){
+            if(!visited[i]){
+                 if(valid(visited,li,i,destination)){
                     return true;
-                }
+                 }
             }
         }
         return false;
